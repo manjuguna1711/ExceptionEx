75dc5a8dd22b3248d96852992a460094986bed82
Manju Parthinathan <manju.parthinathan@yoobeecolleges.com>
2021-06-18 09:56:01 +1200

    Add project files.

---
+++ Exception_Ex/Exception_Ex.cpp
@@ -0,0 +1,49 @@
+// Exception_Ex.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Example : usage of divided by zero exception
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, b;
+    double result =0;
+
+    cout << "\nException - Example \n";
+    cout << "************************\n";
+
+    cout << " Enter a value : ";
+    cin >> a;
+
+    cout << " Enter b value : ";
+    cin >> b;
+
+  try{
+    
+           if (b != 0)
+           {
+
+               result = a / b;                     
+
+               cout << "\n\n a /  b = " << result;
+           }
+          else
+           {
+              throw string("\nPlease provide non zero to b \n");  // trowing the error message when b gets zero  
+           }
+
+  }catch(string error)     // catching the errors throwed
+    {
+     cout << error;        // display error message
+    }
+
+
+    
+   
+
+    cout << "\n\n";
+
+    return 0;
+}
+
